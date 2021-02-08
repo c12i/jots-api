0ef97f83748e5404746ac7da5e12cbe68de117e9
Collins Muriuki <murerwacollins@gmail.com>
2021-02-09 00:40:48 +0300

    2021-02-09_00-40

--- src/index.js
+++ src/index.js
@@ -5,14 +5,51 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  input NewNote {
+    content: String!
+    author: String!
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    createNote(input: NewNote!): Note!
   }
 `;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello'
+    hello: () => 'Hello',
+    notes: () => notes,
+    note: (_parent, { id }) => notes.find(note => note.id === id)
+  },
+  Mutation: {
+    createNote: (_parent, { input }) => {
+      const { content, author } = input;
+      const newNote = {
+        id: String(notes.length + 1),
+        content,
+        author
+      };
+      notes.push(newNote);
+      return newNote;
+    }
   }
 };
 
