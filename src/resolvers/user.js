b611df24cc32a714c0e61f31010a0a591db6d1e1
Collins Muriuki <murerwacollins@gmail.com>
2021-02-11 12:07:09 +0300

    Add user and note resolve fields resolvers

---
+++ src/resolvers/user.js
@@ -0,0 +1,9 @@
+module.exports = {
+  async notes({ _id }, _, { models }) {
+    return await models.Note.find({ author: _id }).sort({ _id: -1 });
+  },
+
+  async favorites({ _id }, _, { models }) {
+    return await models.Note.find({ favoritedBy: _id }).sort({ _id: -1 });
+  }
+};
